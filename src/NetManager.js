ffb589e80dd018a81b6d20bf03d2ecdd13db8d10
Illya9999 <ib66189276@gmail.com>
2020-09-11 14:13:39 -0400

    Fix network errors

--- src/NetManager.js
+++ src/NetManager.js
@@ -2,12 +2,12 @@ module.exports = class NetManager {
 	constructor(DiscordManager, Endpoints){
 		this.block = new Map();
 		this.endpoint = Endpoints.MESSAGE_ACK;
-		Endpoints.MESSAGE_ACK = this.check;
+		let me = this;
+		Endpoints.MESSAGE_ACK = this.check = (channelID, msgID) => {
+			if(me.block.has(channelID+msgID)) return 'http://127.0.0.1:1234';
+			return me.endpoint(channelID, msgID);
+		};
 		this.DiscordManager = DiscordManager;
-	}
-	check(channelID, msgID){
-		if(this.block.has(channelID+msgID)) return 'https://localhost:1234';
-		return this.endpoint(channelID, msgID);
 	}
 	add(channelID, msgID){
 		this.block.set(channelID+msgID, 1);
