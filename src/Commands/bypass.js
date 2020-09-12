9ef794fc517c86ef215320670aff486a40d13363
Illya9999 <ib66189276@gmail.com>
2020-09-12 15:50:24 -0400

    Add bypass command

---
+++ src/Commands/bypass.js
@@ -0,0 +1,23 @@
+import Command from '../Structures/Command';
+import regex from '../../lib/badwords';
+
+const bypassChars = [
+	'\u200a',
+	'\u200b',
+	'\u200c',
+	'\u200d',
+	'\u200e',
+	'\u200f'
+];
+
+const bypass = new Command;
+bypass.setName('bypass')
+	.setDescription('Bypasses most word filters')
+	.setExec((msgData, channel, content) => {
+		msgData.content = content.replace(regex, (match, p1) => {
+			let pos = 1+(Math.random() * (p1.length-1) | 0);
+			return p1.substr(0,pos) + bypassChars[Math.random() * bypassChars.length | 0] + p1.substr(pos);
+		});
+		return msgData;
+	});
+export default bypass;
