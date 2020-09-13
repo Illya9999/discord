7e950966221c7dfd0093fbbaa5acc64a6beef6d1
Illya9999 <ib66189276@gmail.com>
2020-09-12 22:06:16 -0400

    formatting

--- src/Commands/bypass.js
+++ src/Commands/bypass.js
@@ -16,8 +16,8 @@ bypass.setName('bypass')
 	.setDescription('Bypasses most word filters')
 	.setExec((msgData, channel, content) => {
 		msgData.content = content.replace(regex, (match, p1) => {
-			let pos = 1+(Math.random() * (p1.length-1) | 0);
-			return p1.substr(0,pos) + bypassChars[Math.random() * bypassChars.length | 0] + p1.substr(pos);
+			let pos = 1 + (Math.random() * (p1.length - 1) | 0);
+			return p1.substr(0, pos) + bypassChars[Math.random() * bypassChars.length | 0] + p1.substr(pos);
 		});
 		return msgData;
 	})
